2b16bc88da945536f2b26f1d7ebaa778b56beb48
kevcal <kevcal69@gmail.com>
2016-10-10 03:50:32 +0800

    collectstatic

--- staticfiles/js/create.js
+++ staticfiles/js/create.js
@@ -47,6 +47,7 @@ function fetchGroup(){
         'num': parent.find('.num').val(),
         'timein': parent.find('.time_in').val(),
         'timeout': parent.find('.time_out').val(),
+        'meeting': parent.find('.meeting').val()
     }
     $.get('group', data,function(data) {
         data = JSON.parse(data);
@@ -54,7 +55,7 @@ function fetchGroup(){
         resultTable.removeClass('hidden');
         var t = $('.results-body');
         t.empty();
-        data = assignDays(data);
+        data = assignDays(data, (data.timein || 9), (data.timeout || 20), (data.meeting || 4));
         $.each(data, function(i, obj){
             if(obj.time.length > 0) {
                 var time = obj.time;
@@ -66,19 +67,50 @@ function fetchGroup(){
 
                 tr.append('<td>'+ city.join('<br/>') +'</td>');
                 tr.append('<td>'+ time[0]+':00 - ' + time[time.length-1]+':00' +'</td>')
-                tr.append('<td>'+ 1 +'</td>');
+                tr.append('<td>'+ obj.days +'</td>');
                 t.append(tr);
             }
         });
     });
 }
 
-function assignDays(data) {
-    var data = data.sort(function(a, b) {
+function assignDays(data, timeStart, timeDone, meetingPerDay) {
+    data = data.sort(function(a, b) {
         if (a.time[0] < b.time[0]) return -1;
         if (a.time[0] > b.time[0]) return 1;
         return 0;
+    });
+    var dataWithDays = [];
+    var meetingPerDay = 4;
+    var day = 1;
+    var ctr = 0;
+    while (true) {
+        var timeHour = timeStart;
+        var meetings = 0;
+        var index = []
+        for(var i = 0; i < data.length; i++) {
+            var mdat = data[i];
+            if (typeof(mdat.days) === 'undefined' && timeHour <= mdat.time[0]) {
+                timeHour = mdat.time[mdat.time.length-1];
+                mdat.days = day;
+                meetings++;
+            }
+            if (meetingPerDay === meetings) {
+                break;
+            }
+        }
+
+        day += 1;
+        ctr++;
+        var flag = data.filter(function(obj) {return typeof(obj.days) === 'undefined' })
+        if (flag.length === 0 || ctr > 100) {
+            break;
+        }
+    }
+    data = data.sort(function(a, b) {
+        if (a.days < b.days) return -1;
+        if (a.days > b.days) return 1;
+        return 0;
     })
-    console.log(data.length);
     return data;
 }
